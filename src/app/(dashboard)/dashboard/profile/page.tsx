4790bf0fd5363090297567ca2cc8e8aad7a95910
mahnafdev <mahnafdev@gmail.com>
2026-03-06 03:38:57 +0600

    feat(dashboard): create student profile page

---
+++ src/app/(dashboard)/dashboard/profile/page.tsx
@@ -0,0 +1,57 @@
+import { getServerSession } from "@/lib/utils";
+import { Avatar } from "@heroui/react";
+
+const StudentProfile = async () => {
+	// Fetch current user
+	const sessionData = await getServerSession();
+	const { session, user } = await sessionData;
+
+	return (
+		<div className="p-8 flex-1 min-h-screen grid place-items-center">
+			{/* Profile Card */}
+			<div className="min-w-sm p-6 bg-primary-600/10 border border-primary-700 rounded-2xl">
+				{/* Profile Picture */}
+				<Avatar
+					size="lg"
+					className="size-24 rounded-2xl mb-3 mx-auto"
+				>
+					<Avatar.Image
+						src={user.image}
+						alt="Profile Picture"
+					/>
+				</Avatar>
+				{/* Name */}
+				<h2 className="text-3xl font-bold text-center mb-6">{user.name}</h2>
+				<div className="space-y-3">
+					{/* Email */}
+					<div className="flex items-center justify-between gap-x-4">
+						<span className="text-lg">Email</span>
+						<span className="text-zinc-300/75">{user.email}</span>
+					</div>
+					{/* Signed up at */}
+					<div className="flex items-center justify-between gap-x-4">
+						<span className="text-lg">Signed up at</span>
+						<span
+							className="text-zinc-300/75"
+							title={new Date(user.createdAt).toLocaleString()}
+						>
+							{new Date(user.createdAt).toLocaleDateString()}
+						</span>
+					</div>
+					{/* Email */}
+					<div className="flex items-center justify-between gap-x-4">
+						<span className="text-lg">Last logged in at</span>
+						<span
+							className="text-zinc-300/75"
+							title={new Date(session.createdAt).toLocaleString()}
+						>
+							{new Date(session.createdAt).toLocaleDateString()}
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default StudentProfile;

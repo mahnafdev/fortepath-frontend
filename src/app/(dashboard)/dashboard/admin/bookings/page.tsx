96ede05d27ccb3c39461b74c926c0a6d8bc281e7
mahnafdev <mahnafdev@gmail.com>
2026-03-06 23:50:47 +0600

    feat(dashboard): create admin-only bookings page

--- src/app/(dashboard)/dashboard/admin/bookings/page.tsx
+++ src/app/(dashboard)/dashboard/admin/bookings/page.tsx
@@ -1,47 +1,44 @@
 import { TableHeader } from "@/components/shared/TableHeader";
-import { getServerSession } from "@/lib/utils";
+import { Avatar } from "@heroui/react";
 import { cookies } from "next/headers";
-import Image from "next/image";
 
 type Booking = {
 	id: string;
-	tutor: {
-		name: string;
-		image?: string;
-		email: string;
-	};
 	topic: string;
 	dueTime: string;
 	duration: number;
-	status: "CONFIRMED" | "COMPLETED" | "CANCELLED";
 	notes?: string;
+	status: "CONFIRMED" | "COMPLETED" | "CANCELLED";
 	createdAt: string;
+	tutor: {
+		name: string;
+		image: string;
+		email: string;
+	};
+	student: {
+		name: string;
+		image: string;
+		email: string;
+	};
 };
 
-const StudentBookings = async () => {
+const AdminBookings = async () => {
 	// Get cookie store
 	const cookieStore = cookies();
 
-	// Fetch current user
-	const sessionData = await getServerSession();
-	const user = await sessionData.user;
-
-	// Fetch and parse student's bookings
-	const bookingsRes = await fetch(
-		`${process.env.NEXT_PUBLIC_API_URL}/bookings?studentId=${user?.id}`,
-		{
-			headers: {
-				Cookie: (await cookieStore).toString(),
-			},
-			cache: "no-store",
+	// Fetch and parse bookings
+	const bookingsRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/bookings`, {
+		headers: {
+			Cookie: (await cookieStore).toString(),
 		},
-	);
+		cache: "no-store",
+	});
 	const { total: totalBookings, data: bookings } = await bookingsRes.json();
 
 	return (
 		<div className="p-8 flex-1">
 			{/* Header */}
-			<h2 className="text-4xl font-bold text-primary-200 mb-8">Your Bookings</h2>
+			<h2 className="text-4xl font-bold text-primary-200 mb-8">Bookings</h2>
 			{bookings.length > 0 ? (
 				<>
 					{/* Bookings Count */}
@@ -52,13 +49,14 @@ const StudentBookings = async () => {
 					<div className="w-full flex flex-col cursor-default">
 						<TableHeader
 							cols={[
+								"Student",
 								"Tutor",
 								"Topic",
-								"Due Time",
+								"Due Date",
 								"Duration",
 								"Status",
 								"Notes",
-								"Booked At",
+								"Booked On",
 							]}
 						/>
 						{bookings.map((booking: Booking, idx: number) => {
@@ -74,19 +72,29 @@ const StudentBookings = async () => {
 									key={booking.id}
 									className={`${idx % 2 === 0 ? "bg-zinc-900/50" : "bg-zinc-900/65"} flex items-center py-3 text-center ${idx === totalBookings - 1 && "rounded-b-2xl"}`}
 								>
+									{/* Student */}
+									<div
+										className="px-2 flex items-center justify-center gap-x-2 flex-1"
+										title={booking.student.email}
+									>
+										{booking.student.image && (
+											<Avatar size="sm">
+												<Avatar.Image src={booking.student.image} />
+											</Avatar>
+										)}
+										<span className="line-clamp-1">
+											{booking.student.name}
+										</span>
+									</div>
 									{/* Tutor */}
 									<div
 										className="px-2 flex items-center justify-center gap-x-2 flex-1"
 										title={booking.tutor.email}
 									>
 										{booking.tutor.image && (
-											<Image
-												src={booking.tutor.image}
-												alt=""
-												width={24}
-												height={24}
-												className="size-6 rounded-full"
-											/>
+											<Avatar size="sm">
+												<Avatar.Image src={booking.tutor.image} />
+											</Avatar>
 										)}
 										<span className="line-clamp-1">
 											{booking.tutor.name}
@@ -96,9 +104,12 @@ const StudentBookings = async () => {
 									<div className="px-2 flex-1 line-clamp-1">
 										{booking.topic}
 									</div>
-									{/* Due Time */}
-									<div className="px-2 flex-1 line-clamp-1">
-										{new Date(booking.dueTime).toLocaleString()}
+									{/* Due Date */}
+									<div
+										className="px-2 flex-1 line-clamp-1"
+										title={new Date(booking.dueTime).toLocaleString()}
+									>
+										{new Date(booking.dueTime).toLocaleDateString()}
 									</div>
 									{/* Duration */}
 									<div className="px-2 flex-1 line-clamp-1">
@@ -114,9 +125,12 @@ const StudentBookings = async () => {
 									<div className="px-2 flex-1 line-clamp-1">
 										{booking.notes}
 									</div>
-									{/* Booked At */}
-									<div className="px-2 flex-1 line-clamp-1">
-										{new Date(booking.createdAt).toLocaleString()}
+									{/* Booked On */}
+									<div
+										className="px-2 flex-1 line-clamp-1"
+										title={new Date(booking.createdAt).toLocaleString()}
+									>
+										{new Date(booking.createdAt).toLocaleDateString()}
 									</div>
 								</div>
 							);
@@ -126,7 +140,7 @@ const StudentBookings = async () => {
 			) : (
 				<div className="flex flex-col justify-center h-[66vh]">
 					<p className="text-center text-zinc-400 text-xl">
-						You&apos;ve made no bookings yet.
+						No bookings are made yet.
 					</p>
 				</div>
 			)}
@@ -134,4 +148,4 @@ const StudentBookings = async () => {
 	);
 };
 
-export default StudentBookings;
+export default AdminBookings;
